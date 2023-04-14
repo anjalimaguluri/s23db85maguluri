2394e36e9939032215c879b4d9f28c2fff6d66a5
anjalimaguluri <magulurinaganjali@gmail.com>
2023-04-14 01:31:13 -0500

    edited

--- controllers/employee.js
+++ controllers/employee.js
@@ -1,68 +1,144 @@
 var employee = require('../models/employee');
 // List of all employees
-exports.employee_list = function(req, res) {
- res.send('NOT IMPLEMENTED: employee list');
+exports.employee_list = async function (req, res) {
+    try {
+        theEmployees = await employee.find();
+        res.send(theEmployees);
+    }
+    catch (err) {
+        res.status(500);
+        res.send(`{"error": ${err}}`);
+    }
 };
 // for a specific employee.
-exports.employee_detail = function(req, res) {
- res.send('NOT IMPLEMENTED: employee detail: ' + req.params.id);
+exports.employee_detail = async function (req, res) {
+    console.log("detail" + req.params.id)
+    try {
+        result = await employee.findById(req.params.id)
+        res.send(result)
+    } catch (error) {
+        res.status(500)
+        res.send(`{"error": document for id ${req.params.id} not found`);
+    }
 };
 // Handle employee create on POST.
 
-// Handle employee create on POST.
-exports.employee_create_post = async function(req, res) {
+exports.employee_create_post = async function (req, res) {
     console.log(req.body)
     let document = new employee();
     // We are looking for a body, since POST does not have query parameters.
     // Even though bodies can be in many different formats, we will be picky
     // and require that it be a json object
-    // {"employee_type":"goat", "cost":12, "size":"large"}
+    // {"employeetype":"goat", "cost":12, "size":"large"}
     document.employee_Name = req.body.employee_Name;
     document.employee_age = req.body.employee_age;
     document.employee_salary = req.body.employee_salary;
-    try{
-    let result = await document.save();
-    res.send(result);
+    try {
+        let result = await document.save();
+        res.send(result);
     }
-    catch(err){
-    res.status(500);
-    res.send(`{"error": ${err}}`);
+    catch (err) {
+        res.status(500);
+        res.send(`{"error": ${err}}`);
     }
-   };
+};
 
-// exports.employee_create_post = function(req, res) {
-//  res.send('NOT IMPLEMENTED: employee create POST');
-// };
 // Handle employee delete form on DELETE.
-exports.employee_delete = function(req, res) {
- res.send('NOT IMPLEMENTED: employee delete DELETE ' + req.params.id);
-};
-// Handle employee update form on PUT.
-exports.employee_update_put = function(req, res) {
- res.send('NOT IMPLEMENTED: employee update PUT' + req.params.id);
+exports.employee_delete = async function (req, res) {
+    console.log("delete " + req.params.id)
+    try {
+        result = await employee.findByIdAndDelete(req.params.id)
+        console.log("Removed " + result)
+        res.send(result)
+    } catch (err) {
+        res.status(500)
+        res.send(`{"error": Error deleting ${err}}`);
+    }
 };
 
-//List of all employees
-exports.employee_list = async function(req, res) {
-try{
-    theemployees = await employee.find();
-    res.send(theemployees);
-    }
-    catch(err){
-    res.status(500);
-    res.send(`{"error": ${err}}`);
+// Handle employee update form on PUT.
+exports.employee_update_put = async function (req, res) {
+    console.log(`update on id ${req.params.id} with body ${JSON.stringify(req.body)}`)
+    try {
+        let toUpdate = await employee.findById(req.params.id)
+        // Do updates of properties
+        if (req.body.employee_Name) toUpdate.employee_Name = req.body.employee_Name;
+        if (req.body.employee_age) toUpdate.employee_age = req.body.employee_age;
+        if (req.body.employee_salary) toUpdate.employee_salary = req.body.employee_salary;
+        let result = await toUpdate.save();
+        console.log("Sucess " + result)
+        res.send(result)
+    } catch (err) {
+        res.status(500)
+        res.send(`{"error": ${err}: Update for id ${req.params.id} failed`);
     }
 };
+
+
 // VIEWS
 // Handle a show all view
-exports.employee_view_all_Page = async function(req, res) {
+exports.employee_view_all_Page = async function (req, res) {
+    try {
+        theEmployees = await employee.find();
+        res.render('employee', { title: 'employee Search Results', results: theEmployees });
+    }
+    catch (err) {
+        res.status(500);
+        res.send(`{"error": ${err}}`);
+    }
+};
+
+// Handle a show one view with id specified by query
+exports.employee_view_one_Page = async function (req, res) {
+    console.log("single view for id " + req.query.id)
+    try {
+        result = await employee.findById(req.query.id)
+        res.render('employeedetail',
+            { title: 'employee Detail', toShow: result });
+    }
+    catch (err) {
+        res.status(500)
+        res.send(`{'error': '${err}'}`);
+    }
+};
+
+// Handle building the view for creating a employee.
+// No body, no in path parameter, no query.
+// Does not need to be async
+exports.employee_create_Page =  function(req, res) {
+    console.log("create view")
     try{
-    theemployee = await employee.find();
-    res.render('employee', { title: 'employee Search Results', results: theemployee });
+        res.render('employeecreate', { title: 'employee Create'});
     }
     catch(err){
-    res.status(500);
-    res.send(`{"error": ${err}}`);
-    }
-   };
-   
+        res.status(500)
+        res.send(`{'error': '${err}'}`);
+    }
+};
+
+// Handle building the view for updating a employee.
+// query provides the id
+exports.employee_update_Page =  async function(req, res) {
+    console.log("update view for item "+req.query.id)
+    try{
+        let result = await employee.findById(req.query.id)
+        res.render('employeeupdate', { title: 'employee Update', toShow: result });
+    }
+    catch(err){
+        res.status(500)
+        res.send(`{'error': '${err}'}`);
+    }
+};
+
+// Handle a delete one view with id from query
+exports.employee_delete_Page = async function(req, res) {
+    console.log("Delete view for id "  + req.query.id)
+    try{
+        result = await employee.findById(req.query.id)
+        res.render('employeedelete', { title: 'employee Delete', toShow: result });
+    }
+    catch(err){
+        res.status(500)
+        res.send(`{'error': '${err}'}`);
+    }
+};
