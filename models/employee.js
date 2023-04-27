2c5394144677c3167511c41553b222995bfd4089
anjalimaguluri <magulurinaganjali@gmail.com>
2023-04-26 20:48:14 -0500

    changes have been made

--- models/employee.js
+++ models/employee.js
@@ -1,7 +1,14 @@
 const mongoose = require("mongoose")
 const employeeSchema = mongoose.Schema({
-employee_Name: String,
-employee_age: String,
-employee_salary: Number
+    employee_Name: {
+        type: String,
+        required: [true, "Name required"]
+    },
+    employee_age: Number,
+    employee_salary:  {
+        type: Number,
+        min: [1000, "Min value is 1000"],
+        max: [6000, "Max value is 6000"]
+    }
 })
 module.exports = mongoose.model("employee",employeeSchema)
--- routes/employee.js
+++ routes/employee.js
@@ -6,9 +6,9 @@ router.get('/', employee_controlers.employee_view_all_Page );
 module.exports = router;
 
 router.get('/detail', employee_controlers.employee_view_one_Page);
-router.get('/create',employee_controlers.employee_create_Page);
-router.get('/update',employee_controlers.employee_update_Page);
-router.get('/delete',employee_controlers.employee_delete_Page);
+//router.get('/create',employee_controlers.employee_create_Page);
+//router.get('/update',employee_controlers.employee_update_Page);
+//router.get('/delete',employee_controlers.employee_delete_Page);
 
 // var express = require('express');
 // var router = express.Router();
@@ -19,3 +19,17 @@ router.get('/delete',employee_controlers.employee_delete_Page);
 // });
 
 // module.exports = router;
+
+// A little function to check if we have an authorized user and continue on
+//or
+// redirect to login.
+const secured = (req, res, next) => {
+ if (req.user){
+ return next();
+ }
+ req.session.returnTo = req.originalUrl;
+ res.redirect("/login");
+ }
+ router.get('/create',secured,employee_controlers.employee_create_Page);
+router.get('/update',secured,employee_controlers.employee_update_Page);
+router.get('/delete',secured,employee_controlers.employee_delete_Page);
